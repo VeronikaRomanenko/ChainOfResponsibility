89a71b8d0f8fa3f98e87ca38a02701e9c5e10715
VeronikaRomanenko <Veronika.Romanenko333@gmail.com>
2019-05-31 16:55:03 +0300

    Commit

---
+++ ChainOfResponsibility/ChainOfResponsibility/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ChainOfResponsibility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbWeight = new System.Windows.Forms.TextBox();
+            this.txbDiameter = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txbSum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вес";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Диаметр";
+            // 
+            // txbWeight
+            // 
+            this.txbWeight.Location = new System.Drawing.Point(12, 50);
+            this.txbWeight.Name = "txbWeight";
+            this.txbWeight.Size = new System.Drawing.Size(159, 20);
+            this.txbWeight.TabIndex = 2;
+            // 
+            // txbDiameter
+            // 
+            this.txbDiameter.Location = new System.Drawing.Point(12, 124);
+            this.txbDiameter.Name = "txbDiameter";
+            this.txbDiameter.Size = new System.Drawing.Size(159, 20);
+            this.txbDiameter.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(177, 63);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(168, 71);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txbSum
+            // 
+            this.txbSum.Location = new System.Drawing.Point(73, 203);
+            this.txbSum.Name = "txbSum";
+            this.txbSum.ReadOnly = true;
+            this.txbSum.Size = new System.Drawing.Size(201, 20);
+            this.txbSum.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Сумма";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 263);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbSum);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txbDiameter);
+            this.Controls.Add(this.txbWeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbWeight;
+        private System.Windows.Forms.TextBox txbDiameter;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txbSum;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChainOfResponsibility/ChainOfResponsibility/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChainOfResponsibility
+{
+    public partial class Form1 : Form
+    {
+        Coin coin_5 = new Coin_5();
+        Coin coin_10 = new Coin_10();
+        Coin coin_25 = new Coin_25();
+        Coin coin_50 = new Coin_50();
+        Coin coin_100 = new Coin_100();
+        public Form1()
+        {
+            InitializeComponent();
+            coin_5.Successor = coin_10;
+            coin_10.Successor = coin_25;
+            coin_25.Successor = coin_50;
+            coin_50.Successor = coin_100;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            coin_5.HandleRequest(int.Parse(txbWeight.Text), int.Parse(txbDiameter.Text));
+            txbSum.Text = $"{Sum.sum} коп.";
+        }
+    }
+    public static class Sum
+    {
+        public static int sum = 0;
+    }
+    abstract class Coin
+    {
+        public Coin Successor { get; set; }
+        public abstract void HandleRequest(int weight, int diameter);
+    }
+    class Coin_5 : Coin
+    {
+        public override void HandleRequest(int weight, int diameter)
+        {
+            if (Math.Abs(10 - weight) <= 1 && Math.Abs(5 - diameter) <= 1)
+            {
+                MessageBox.Show("Монета номиналом 5 коп. принята");
+                Sum.sum += 5;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(weight, diameter);
+            }
+            else
+            {
+                MessageBox.Show("Монета не принята");
+            }
+        }
+    }
+    class Coin_10 : Coin
+    {
+        public override void HandleRequest(int weight, int diameter)
+        {
+            if (Math.Abs(15 - weight) <= 1 && Math.Abs(8 - diameter) <= 1)
+            {
+                MessageBox.Show("Монета номиналом 10 коп. принята");
+                Sum.sum += 10;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(weight, diameter);
+            }
+            else
+            {
+                MessageBox.Show("Монета не принята");
+            }
+        }
+    }
+    class Coin_25 : Coin
+    {
+        public override void HandleRequest(int weight, int diameter)
+        {
+            if (Math.Abs(20 - weight) <= 1 && Math.Abs(11 - diameter) <= 1)
+            {
+                MessageBox.Show("Монета номиналом 25 коп. принята");
+                Sum.sum += 25;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(weight, diameter);
+            }
+            else
+            {
+                MessageBox.Show("Монета не принята");
+            }
+        }
+    }
+    class Coin_50 : Coin
+    {
+        public override void HandleRequest(int weight, int diameter)
+        {
+            if (Math.Abs(25 - weight) <= 1 && Math.Abs(14 - diameter) <= 1)
+            {
+                MessageBox.Show("Монета номиналом 50 коп. принята");
+                Sum.sum += 50;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(weight, diameter);
+            }
+            else
+            {
+                MessageBox.Show("Монета не принята");
+            }
+        }
+    }
+    class Coin_100 : Coin
+    {
+        public override void HandleRequest(int weight, int diameter)
+        {
+            if (Math.Abs(30 - weight) <= 1 && Math.Abs(17 - diameter) <= 1)
+            {
+                MessageBox.Show("Монета номиналом 1 грн. принята");
+                Sum.sum += 100;
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(weight, diameter);
+            }
+            else
+            {
+                MessageBox.Show("Монета не принята");
+            }
+        }
+    }
+}
